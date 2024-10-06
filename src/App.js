441b4616f645cd7d614867ec0418a9763d350d85
Bryam <bryam.valbuenal@uniagustiniana.edu.co>
2024-10-06 18:43:20 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TitleWithVideo from './components/Jsx/TitleWithVideo';
+import HeroSection from './components/Jsx/HeroSection';
+import Footer from './components/Jsx/Footer';
+import Header from './components/Jsx/Header';
+import Torneos from './components/Jsx/Torneos'; // Importar la página de Torneos
+import Formulario from './components/Jsx/Formulario'; // Importar el componente Formulario
+import Clases from './components/Jsx/Clases';
+import ClasesFormulario from './components/Jsx/ClasesFormulario';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={
+            <>
+              <TitleWithVideo />
+              <HeroSection />
+            </>
+          } />
+          
+          <Route path="/torneos" element={<Torneos />} />
+          <Route path="/formulario" element={<Formulario />} /> {/* Añadir esta línea */}
+          <Route path="/clases" element={<Clases />} />
+          <Route path="/ClasesFormulario" element={<ClasesFormulario />} />
+        </Routes>
+
+        <Footer /> {/* Mantén el footer en todas las rutas */}
+      </div>
+    </Router>
   );
 }
 
